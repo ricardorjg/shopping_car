2a7be577a13804a1b4216b4765a9f7dedb96a3a9
Ricardo Jose Gomez <rjgdevapp@gmail.com>
2020-02-16 08:41:52 -0500

    uibuild

---
+++ build/precache-manifest.11a0daf18a0dd40c18c09017d4f0c723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf88a533990e609019bc324cb9a4a92c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "743ce6a2e8db2a3eb603",
+    "url": "/static/css/main.7c825148.chunk.css"
+  },
+  {
+    "revision": "2e99471eee3922237ab5",
+    "url": "/static/js/2.5142a6a0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.5142a6a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "743ce6a2e8db2a3eb603",
+    "url": "/static/js/main.832caff3.chunk.js"
+  },
+  {
+    "revision": "cea84273a7b0155595e3",
+    "url": "/static/js/runtime-main.82f41bb7.js"
+  }
+]);
